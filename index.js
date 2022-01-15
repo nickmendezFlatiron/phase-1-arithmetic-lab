767f156ece608538b9c9697dc5bfb8e9719ca863
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-15 14:43:55 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.ceil(Math.random());
+random;
+
+let num3 = 20 , num4 = 8;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(20, 19, 2, 4, 5);
+max;
